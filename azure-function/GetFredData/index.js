0360ff2bffcd3ca670c3db5ef39bf3fa3a053a61
Matt Moore <mmoore11@zagmail.gonzaga.edu>
2023-05-06 13:59:01 -0700

    Initializing this repo with configurations for my economic azure function

---
+++ azure-function/GetFredData/index.js
@@ -0,0 +1,111 @@
+const axios = require('axios');
+const { Connection, Request } = require('tedious');
+require('dotenv').config();
+
+module.exports = async function (context, req) {
+  try {
+    // Fetch data from FRED API
+    const unemploymentData = await fetchData('UNRATE');
+    const cpiInflationData = await fetchData('CPIAUCSL');
+    const gdpPerCapitaData = await fetchData('A939RX0Q048SBEA');
+    const interestRatesData = await fetchData('DFF');
+    const housingInflationData = await fetchData('USSTHPI');
+    const sp500Data = await fetchData('SP500');
+
+    // Store data in Azure Database
+    await storeDataInAzureDB(context, [
+      unemploymentData,
+      cpiInflationData,
+      gdpPerCapitaData,
+      interestRatesData,
+      housingInflationData,
+      sp500Data,
+    ]);
+
+    context.res = {
+      status: 200,
+      body: 'Data stored successfully',
+    };
+    console.log('Data stored successfully')
+  } catch (error) {
+    context.res = {
+      status: 500,
+      body: 'An error occurred while storing data',
+    };
+    context.log.error(error);
+    console.error('An error occurred while storing data')
+  }
+};
+
+async function fetchData(seriesId) {
+  const apiKey = process.env.FRED_API_KEY
+  const url = `https://api.stlouisfed.org/fred/series/observations?series_id=${seriesId}&api_key=${apiKey}&file_type=json`;
+
+  const response = await axios.get(url);
+  const observations = response.data.observations;
+
+  // Extract the latest value from the observations
+  const latestObservation = observations[observations.length - 1];
+  const { date, value } = latestObservation;
+
+  return { seriesId, date, value };
+}
+
+async function storeDataInAzureDB(context, data) {
+  const azureConfig = {
+    server: 'final-economic-server',
+    authentication: {
+      type: 'default',
+      options: {
+        userName: process.env.AZURE_DB_USER,
+        password: process.env.AZURE_DB_PASS,
+      },
+    },
+    options: {
+      databaseName: 'main_fred_db',
+      encrypt: true,
+    },
+  };
+
+  const connection = new Connection(azureConfig);
+
+  connection.on('connect', (err) => {
+    if (err) {
+      context.log.error(err);
+      throw err;
+    } else {
+      insertData(context, connection, data);
+    }
+  });
+
+  connection.connect();
+}
+
+function insertData(context, connection, data) {
+  const table = 'FredSeriesData'; // Replace with the name of your database table
+
+  const insertQuery = `
+    INSERT INTO ${table} (SeriesId, Date, Value)
+    VALUES (@seriesId, @date, @value);
+  `;
+
+  const request = new Request(insertQuery, (err) => {
+    if (err) {
+      context.log.error(err);
+      throw err;
+    } else {
+      context.log('Data inserted successfully');
+      console.log('Data inserted successfully')
+      connection.close();
+    }
+  });
+
+  data.forEach((item) => {
+    request.addParameter('seriesId', TYPES.NVarChar, item.seriesId);
+    request.addParameter('date', TYPES.Date, new Date(item.date));
+    request.addParameter('value', TYPES.Float, item.value);
+
+    connection.execSql(request);
+    request.parameters = [];
+  });
+}
