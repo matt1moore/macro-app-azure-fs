a7f048a51bb1a824b3279e0ce6ee45b3385d77f6
Matt Moore <mmoore11@zagmail.gonzaga.edu>
2023-05-08 14:58:40 -0700

    Fixing the following error:     Handling WorkerErrorEvent for runtime:node, workerId:node. Failed with: Microsoft.Azure.WebJobs.Script.Workers.WorkerProcessExitException: node exited with code 1 (0x1) ---> System.Exception: LanguageWorkerConsoleLog[error] Worker uncaught exception (learn more: https://go.microsoft.com/fwlink/?linkid=2097909 ): ReferenceError: TYPES is not defined at /home/site/wwwroot/GetFredData/index.js:109:38 at Array.forEach (<anonymous>) at insertData (/home/site/wwwroot/GetFredData/index.js:108:8) at Connection.<anonymous> (/home/site/wwwroot/GetFredData/index.js:81:7) at Connection.emit (node:events:513:28) at Connection.emit (/home/site/wwwroot/node_modules/tedious/lib/connection.js:1040:18) at Connection.processedInitialSql (/home/site/wwwroot/node_modules/tedious/lib/connection.js:1665:10) at /home/site/wwwroot/node_modules/tedious/lib/connection.js:2719:14 at process.processTicksAndRejections (node:internal/process/task_queues:95:5) --- End of inner exception stack trace --- by importing the TYPES attribute with tedious

--- azure-function/GetFredData/index.js
+++ azure-function/GetFredData/index.js
@@ -1,5 +1,6 @@
 const axios = require('axios');
 const { Connection, Request } = require('tedious');
+const { TYPES } = require('tedious')
 require('dotenv').config();
 
 module.exports = async function (context, req) {
