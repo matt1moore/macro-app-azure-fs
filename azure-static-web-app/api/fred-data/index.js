9cac2d57f6214455abeee81b85af385775546820
Matt Moore <mmoore11@zagmail.gonzaga.edu>
2023-05-10 09:12:29 -0700

    Added packaging info for the api, hoping to fix issues there

---
+++ azure-static-web-app/api/fred-data/index.js
@@ -0,0 +1,42 @@
+const azure = require('azure-functions-core-tools');
+const sql = require('mssql');
+
+module.exports = async function (context, req) {
+  // Azure SQL Database configuration
+  const config = {
+    server: 'your-server-name.database.windows.net',
+    database: 'your-database-name',
+    user: 'your-username',
+    password: 'your-password',
+    options: {
+      encrypt: true,
+      trustServerCertificate: false
+    }
+  };
+
+  try {
+    // Connect to the Azure SQL Database
+    await sql.connect(config);
+
+    // Query to retrieve economic indicator data
+    const query = 'SELECT * FROM EconomicIndicators';
+
+    // Execute the query
+    const result = await sql.query(query);
+
+    // Return the retrieved data as JSON response
+    context.res = {
+      status: 200,
+      body: result.recordset
+    };
+  } catch (error) {
+    console.error('Error retrieving data from Azure SQL Database:', error);
+    context.res = {
+      status: 500,
+      body: 'Internal Server Error'
+    };
+  } finally {
+    // Close the database connection
+    sql.close();
+  }
+};
